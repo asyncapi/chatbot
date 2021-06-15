045e744e0e5f00cc357df885fa1928c821c907fe
AceTheCreator <devaze007@gmail.com>
2021-06-15 04:16:08 +0100

    setup test client for interaction

--- server/src/helpers/socket.js
+++ server/src/helpers/socket.js
@@ -2,7 +2,17 @@ import server from '../server';
 
 const io = require('socket.io')(server);
 
-const client = io.on('connection', async (client) => {
-  console.log(client);
-  console.log('hello');
-});
+export default function startSocket() {
+  io.on('connection', async (client) => {
+    client.on('signIn', (evt) => {
+      client.to(client.id).emit('bot-message', 'jdbdkbkbkbk');
+    });
+    client.on('message', (evt) => {
+      console.log(evt.data);
+      client.to(client.id).emit('message', evt);
+    });
+  });
+  io.on('disconnect', (evt) => {
+    console.log('someone left');
+  });
+}
--- server/src/start.js
+++ server/src/start.js
@@ -1,6 +1,8 @@
 import server from './server';
+import startSocket from './helpers/socket';
 
 const startServer = async () => {
+  startSocket();
   if (process.env.NODE_ENV === 'development') {
     server.listen(5000, () => {
       console.log('app now listening to port 5000');
