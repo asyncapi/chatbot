ac014a1a0c78708b19c85b1ae1dfa44509a5202c
AceTheCreator <devaze007@gmail.com>
2021-06-22 14:29:14 +0100

    writing document validation

--- server/src/controllers/message.js
+++ server/src/controllers/message.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-plusplus */
 /* eslint-disable consistent-return */
 import client from '../helpers/wit';
-import { firstEntityValue } from '../utils/entities';
+import { firstEntityValue, childEntityValue } from '../utils/entities';
 import questions from '../models/questions';
 import defaultSpec from '../models/defaultSpec.json';
 import answers from '../models/answers.json';
@@ -9,23 +10,60 @@ const counter = {
   parent: 0,
   child: 0,
 };
+const document = defaultSpec;
 
 const messageHandler = (data, socket, io) => {
   client
     .message(data, {})
     .then((res) => {
-      console.log(res);
-      console.log(res.entities);
       const wantToGenerate = firstEntityValue(res.entities, 'generator_flow:generator_flow');
       const question = firstEntityValue(res.entities, 'question:question');
       if (wantToGenerate) {
-        if (counter.child >= questions[counter.parent].questions.length) {
+        if (
+          counter.child >= questions[counter.parent].questions.length
+        ) {
           counter.parent += 1;
           counter.child = 0;
         }
         const toAsk = questions[counter.parent];
         const ask = toAsk.questions[counter.child];
+        console.log(counter);
+        const generateEntities = childEntityValue(
+          res.entities,
+          'generator_flow:generator_flow',
+        );
+        if (generateEntities) {
+          if (
+            generateEntities.name === 'skip'
+                    && generateEntities.confidence > 0.5
+          ) {
+            if (ask.required) {
+              return io
+                .to(socket.id)
+                .emit(
+                  'message',
+                  "You can't skip this question because it's required",
+                );
+            }
+            io.to(socket.id).emit('message', "Ok let's move on");
+          }
+          if (
+            ask.type === 'string'
+                        && generateEntities.role !== 'message_body'
+          ) {
+            return io
+              .to(socket.id)
+              .emit(
+                'message',
+                'A valid application name should be an alphabet',
+              );
+          }
+        }
+        if (toAsk.text) {
+          io.to(socket.id).emit('message', toAsk.text);
+        }
         io.to(socket.id).emit('message', ask.text);
+        console.log(counter);
         counter.child++;
         return;
       }
--- server/src/utils/entities.js
+++ server/src/utils/entities.js
@@ -12,3 +12,18 @@ export const firstEntityValue = (entities, entity) => {
 
   return val;
 };
+
+export const childEntityValue = (entities, entity) => {
+  const val = entities
+    && entities[entity]
+    && Array.isArray(entities[entity])
+    && entities[entity].length > 0
+    && entities[entity][0].entities.length > 0
+    && entities[entity][0].entities[0];
+
+  if (!val) {
+    return null;
+  }
+
+  return val;
+};
