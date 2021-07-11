488f01e749d73a742fcb102ea69ec8c101035368
AceTheCreator <devaze007@gmail.com>
2021-07-11 01:48:31 +0100

    removed .env file

--- server/src/start.js
+++ server/src/start.js
@@ -1,10 +1,10 @@
 import server from './server';
 import startSocket from './helpers/socket';
-
+const PORT = process.env.PORT || 5000;
 const startServer = async () => {
   startSocket();
   if (process.env.NODE_ENV === 'development') {
-    server.listen(5000, () => {
+    server.listen(PORT, () => {
       console.log('app now listening to port 5000');
     });
   }
