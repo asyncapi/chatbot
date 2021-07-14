e39c95729c7e2118be3c4ba53a8587dc8628e293
Ace <40604284+AceTheCreator@users.noreply.github.com>
2021-07-14 11:33:14 +0100

    Update server/src/lib/events.js
    Co-authored-by: Maciej Urbańczyk <urbanczyk.maciej.95@gmail.com>

--- server/src/lib/events.js
+++ server/src/lib/events.js
@@ -1,11 +1,7 @@
 const EventEmitter = require('events');
 
-const events = module.exports;
-const eventEmitter = new EventEmitter();
+class CustomEventEmitter extends EventEmitter {
+  names = this.eventNames;
+}
 
-events.on = eventEmitter.on.bind(eventEmitter);
-events.once = eventEmitter.once.bind(eventEmitter);
-events.emit = (eventName, eventPayload) => {
-  eventEmitter.emit(eventName, eventPayload);
-};
-events.names = eventEmitter.eventNames.bind(eventEmitter);
+module.exports = new CustomEventEmitter();
