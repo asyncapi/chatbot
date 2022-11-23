09bd5acf471e7479c73f08ae5570fa584d6a8f5f
acethecreator <devaze007@gmail.com>
2022-11-23 04:14:13 +0100

    .

--- packages/server/src/utils/intents.js
+++ packages/server/src/utils/intents.js
@@ -1,6 +1,7 @@
+/* eslint-disable import/prefer-default-export */
 export const getIntent = (data) => {
   if (data && Object.keys(data).length > 0) {
-    return data[0]
+    return data[0];
   }
   return null;
 };
